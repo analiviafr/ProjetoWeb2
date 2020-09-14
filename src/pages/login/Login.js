88c6140ce3ca89a5a608daad22f67f08917651a9
Ana Lívia Franco <analivia@live.com>
2020-09-14 18:40:53 -0300

    Login style

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,55 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
-import {Formik, Form, Field} from 'formik';
 import axios from 'axios';
+import './Login.css';
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(['']);
-    const [flag, setFlag] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(['']);
+  const [flag, setFlag] = useState('');
 
-    async function onLoginSubmit(e) {
-        e.preventDefault();
-        if(!email){
-            setError('E-mail inválido');
-            return;
-        }
-        if(!password){
-            setError('Senha inválida');
-            return;
-        }
-
-
-        try {
-            const res = await axios.post('https://reqres.in/api/login', {"email": email,"password": password});
-            localStorage.setItem('app-token', res.data.token);
-            setError('');
-            setFlag(localStorage.getItem('app-token'));
-        } catch{
-            setError('Endereço de email ou senha inválido.');
-            return;
-        }
+  async function onLoginSubmit(e) {
+    e.preventDefault();
+    if(!email){
+      setError('E-mail inválido');
+      return;
+    }
+    if(!password){
+      setError('Senha inválida');
+      return;
     }
 
-    useEffect(() => {
-        setFlag(localStorage.getItem('app-token'));
-    },[])
 
-    return (
-        <div className="Login">
-          <h1>Login</h1>
-          <Link to="/" className="Voltar-btn" >Voltar</Link>
+    try {
+      const res = await axios.post('https://reqres.in/api/login', {"email": email,"password": password});
+      localStorage.setItem('app-token', res.data.token);
+      setError('');
+      setFlag(localStorage.getItem('app-token'));
+    } catch{
+      setError('Endereço de email ou senha inválido.');
+      return;
+    }
+  }
 
-          <form onSubmit={onLoginSubmit}>
-            <label htmlFor="email">E-mail:</label>
-            <input id="email" type="email" value={email} onChange={e => setEmail(e.target.value)}/>
-            <label htmlFor="password">Senha: </label>
-            <input id="password" type="password" value={password} onChange={e => setPassword(e.target.value)}/>
-            <button className="Login-btn" type="submit">Entrar</button>
-          </form>
+  useEffect(() => {
+    setFlag(localStorage.getItem('app-token'));
+  },[])
 
-          {error&&<span  className="Login-error">{error}</span>}
-        </div>
-    );
+
+  return (
+    <div className="Login">
+      <h1>Login</h1>
+      <form onSubmit={onLoginSubmit}>
+
+        <input id="email" type="email" value={email} placeholder="Digite seu e-mail" onChange={e => setEmail(e.target.value)}/>
+        <br/>
+
+        <input id="password" type="password" value={password} placeholder="Digite sua senha" onChange={e => setPassword(e.target.value)}/>
+        <br/>
+
+        <Link to="/"><button className="Voltar-btn">Voltar</button></Link>
+        <button className="Login-btn" type="submit">Entrar</button>
+        </form>
+
+        {error&&<span  className="Login-error">{error}</span>}
+    </div>
+  );
 }
