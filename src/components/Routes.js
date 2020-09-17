617e31f37f4d78cd54ba0325c29aa14dafdf552d
Maria Victória Uzum <mvic.uzum@gmail.com>
2020-09-17 16:12:41 -0300

    Deploy

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -8,30 +8,16 @@ import Home from '../pages/home';
 import NotFound from './NotFound';
 import PrivateRoute from './PrivateRoute';
 
-function Routes() {
-  return(
-    <div>
-      <BrowserRouter basename=process.env.PUBLIC_URL}>
-        <Switch>
-          <Route exact path="/login" component={Login}/>
-          <Route exact path="/register" component={Register}/>
-          <PrivateRoute path="/search" component={Search}  />
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound}/>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
-}
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/register" component={Register}/>
+      <PrivateRoute path="/search" component={Search}  />
+      <Route exact path="/ProjetoWeb2" component={Home} />
+      <Route component={NotFound}/>
+    </Switch>
+  </BrowserRouter>
+)
 
 export default Routes;
-/src/components/Routes.js
-  Line 12:25:  Parsing error: Unexpected token
-
-  10 |
-  11 | const Routes = () => (
-> 12 |   <BrowserRouter process.env.PUBLIC_URL}>
-     |                         ^
-  13 |     <Switch>
-  14 |       <Route exact path="/login" component={Login}/>
-  15 |       <Route exact path="/register" component={Register}/>
