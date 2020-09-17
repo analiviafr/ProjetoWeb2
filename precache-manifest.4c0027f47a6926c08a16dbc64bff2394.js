22c08a2b56847b1426aab75698d247c517503b58
Maria Victória Uzum <mvic.uzum@gmail.com>
2020-09-17 13:03:27 -0300

    Updates

---
+++ precache-manifest.4c0027f47a6926c08a16dbc64bff2394.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0feddfddd91ef9703026e40520eb8032",
+    "url": "/ProjetoWeb2/index.html"
+  },
+  {
+    "revision": "ab380ecf3ab1a957c930",
+    "url": "/ProjetoWeb2/static/css/main.1fba1d71.chunk.css"
+  },
+  {
+    "revision": "1c01188bc9442ad32f79",
+    "url": "/ProjetoWeb2/static/js/2.aa1d6516.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ProjetoWeb2/static/js/2.aa1d6516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab380ecf3ab1a957c930",
+    "url": "/ProjetoWeb2/static/js/main.a7ae05ec.chunk.js"
+  },
+  {
+    "revision": "2770fcc0fa48ddc83769",
+    "url": "/ProjetoWeb2/static/js/runtime-main.1f902b6c.js"
+  },
+  {
+    "revision": "87acf7acf78528274728973c2ddc513e",
+    "url": "/ProjetoWeb2/static/media/Appleicon.87acf7ac.PNG"
+  },
+  {
+    "revision": "49666365de343b03d6b2d6299d94c4bb",
+    "url": "/ProjetoWeb2/static/media/Googleicon.49666365.PNG"
+  },
+  {
+    "revision": "63c2c4c28143a2ccacb1a84f64b52538",
+    "url": "/ProjetoWeb2/static/media/fundo1.63c2c4c2.jpg"
+  },
+  {
+    "revision": "db4260c44ea67b03aa2c33c2a14ef748",
+    "url": "/ProjetoWeb2/static/media/fundo1_mobile.db4260c4.jpg"
+  },
+  {
+    "revision": "8069056bec992d94de56d18020d31673",
+    "url": "/ProjetoWeb2/static/media/fundo2.8069056b.png"
+  },
+  {
+    "revision": "1abf1307c0c5ed4932efa726ddae28a8",
+    "url": "/ProjetoWeb2/static/media/fundo3.1abf1307.png"
+  },
+  {
+    "revision": "bdfb91be11b15653819e8f9bb2c60c8e",
+    "url": "/ProjetoWeb2/static/media/fundo4.bdfb91be.png"
+  },
+  {
+    "revision": "59060dcafb8e4b4ccb1a3d3fb84e5a4a",
+    "url": "/ProjetoWeb2/static/media/img12.59060dca.jpg"
+  },
+  {
+    "revision": "92deb4d4b0b967e59945626813273154",
+    "url": "/ProjetoWeb2/static/media/img3.92deb4d4.png"
+  },
+  {
+    "revision": "db3e397a41dbf447e5877b503eeb5862",
+    "url": "/ProjetoWeb2/static/media/img3_mobile.db3e397a.png"
+  },
+  {
+    "revision": "10a21ac659550fdd6c2941cd7ec275ab",
+    "url": "/ProjetoWeb2/static/media/img4.10a21ac6.png"
+  },
+  {
+    "revision": "82fb8a48a1c48e1af52bea8bf8e799b9",
+    "url": "/ProjetoWeb2/static/media/img5.82fb8a48.png"
+  },
+  {
+    "revision": "b0b781c163dae4ea5281173ea33043f8",
+    "url": "/ProjetoWeb2/static/media/img6.b0b781c1.png"
+  },
+  {
+    "revision": "8069056bec992d94de56d18020d31673",
+    "url": "/ProjetoWeb2/static/media/img7.8069056b.png"
+  },
+  {
+    "revision": "1abf1307c0c5ed4932efa726ddae28a8",
+    "url": "/ProjetoWeb2/static/media/img8.1abf1307.png"
+  },
+  {
+    "revision": "90d8f1af934daaa634246c79f5a7ea4f",
+    "url": "/ProjetoWeb2/static/media/logo.90d8f1af.png"
+  },
+  {
+    "revision": "a0e4a2fa257cd10dc5b4fd2dcd6a3194",
+    "url": "/ProjetoWeb2/static/media/logo2.a0e4a2fa.png"
+  }
+]);
