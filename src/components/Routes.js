88e8f99b993852992618df0815e272d29f0af01a
Maria Victória Uzum <mvic.uzum@gmail.com>
2020-09-17 15:58:49 -0300

    Update Routes.js

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -8,17 +8,19 @@ import Home from '../pages/home';
 import NotFound from './NotFound';
 import PrivateRoute from './PrivateRoute';
 
-const Routes = () => (
-  <BrowserRouter basename=process.env.PUBLIC_URL}>
-    <Switch>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/register" component={Register}/>
-      <PrivateRoute path="/search" component={Search}  />
-      <Route exact path="/" component={Home} />
-      <Route component={NotFound}/>
-    </Switch>
-  </BrowserRouter>
-)
+function Routes() {
+  return(
+    <BrowserRouter basename=process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+        <PrivateRoute path="/search" component={Search}  />
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound}/>
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
 export default Routes;
 /src/components/Routes.js
