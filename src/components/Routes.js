1d52fb83c7c6093f5a1a5b42ab68aafef5a27504
Maria Victória Uzum <mvic.uzum@gmail.com>
2020-09-17 14:25:40 -0300

    Testes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -11,10 +11,10 @@ import PrivateRoute from './PrivateRoute';
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/register" component={Register}/>
-      <PrivateRoute path="/search" component={Search}  />
-      <Route exact path="/" component={Home} />
+      <Route exact path="ProjetoWeb2/login" component={Login}/>
+      <Route exact path="ProjetoWeb2/register" component={Register}/>
+      <PrivateRoute path="ProjetoWeb2/search" component={Search}  />
+      //<Route exact path="/" component={Home} />
       <Route exact path="/ProjetoWeb2/" component={Home} />
       <Route component={NotFound}/>
     </Switch>
