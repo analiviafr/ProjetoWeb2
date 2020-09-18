539898e11a16c11d8fceac9e4a7a2a0ebf292bde
Ana Lívia Franco <analivia@live.com>
2020-09-18 11:39:57 -0300

    Update Routes.js

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -10,11 +10,13 @@ import PrivateRoute from './PrivateRoute';
 
 const Routes = () => (
   <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Route exact path="/" exact component={Home}/>
+    <Switch>
       <Route exact path="/ProjetoWeb2/login" component={Login}/>
       <Route exact path="/ProjetoWeb2/register" component={Register}/>
       <PrivateRoute path="/ProjetoWeb2/search" component={Search}/>
+      <Route exact path="/" exact component={Home}/>
       <Route component={NotFound}/>
+    </Switch>
   </BrowserRouter>
 )
 
